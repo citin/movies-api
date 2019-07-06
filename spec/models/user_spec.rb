fd5760d41107845e90affb79913c1182967461d8
Andres Bernardi <cito.bernardi@gmail.com>
2019-07-06 18:36:23 +0200

    adds test files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.create(email: "user_test@testing.com") }
+  let(:movie_content) do 
+    Movie.create(
+      title: "Perdido en ti", 
+      plot: "Lorem ipsum dolor sit amet.")
+  end
+  let(:purchase_option_hd) { PurchaseOption.create(price: 2.99, video_quality: "HD", content: movie_content) }
+
+  context "associations" do
+    it { should have_many(:purchases) } 
+  end
+
+  it "can purchase a content" do
+    user.purchase(purchase_option_hd)
+
+    expect(user.library.size).to eql(1)
+  end
+
+  it "exclude expired purchase from libray" do
+    user.purchase(purchase_option_hd)
+
+    user.purchases.first.update(created_at: 3.days.ago)
+
+    expect(user.library.size).to eql(0)
+  end
+
+  it "can't purchase a content that is in the library" do
+    user.purchase(purchase_option_hd)
+
+    expect{ user.purchase(purchase_option_hd) }.to raise_error(User::AlreadyPurchasedError)
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,3 +59,12 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+
+# shoulda-matchers config
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
