1831433f1561d7135e6dd6ca0416e5d39a97756b
Andres Bernardi <cito.bernardi@gmail.com>
2019-07-12 13:34:14 +0200

    adds callbacks in users api controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::UsersController < ApplicationController
 
+  before_action :set_user
+  before_action :set_purchase_option, only: [:purchase]
+
   rescue_from User::AlreadyPurchasedError, with: :render_method_not_allowed
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
@@ -14,14 +17,22 @@ class Api::V1::UsersController < ApplicationController
   # POST /api/v1/users/:id/purchase
   # Params: { "purchase_option_id": 1 }
   def purchase
-    # todo: before_action
-    purchase_option = PurchaseOption.find(params[:purchase_option_id])
-    User.find(params[:id]).purchase(purchase_option)
-    render status: 200
+    @user.purchase(@purchase_option)
+    render json: @user.library
   end
 
   # GET /api/v1/users/:id/purchases
   def purchases
-    render json: User.find(params[:id]).library
+    render json: @user.library
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_purchase_option
+    @purchase_option = PurchaseOption.find(params[:purchase_option_id])    
   end
 end
