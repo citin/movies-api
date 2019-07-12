d3c1a2a12de396008419894e4ada3e0961c258b9
Andres Bernardi <cito.bernardi@gmail.com>
2019-07-12 13:35:07 +0200

    handle exceptions in application controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,17 +3,6 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user
   before_action :set_purchase_option, only: [:purchase]
 
-  rescue_from User::AlreadyPurchasedError, with: :render_method_not_allowed
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
-  def render_not_found_response(exception)
-    render json: { error: exception.message }, status: :not_found
-  end
-
-  def render_method_not_allowed(exception)
-    render json: { error: exception.message }, status: 405
-  end
-
   # POST /api/v1/users/:id/purchase
   # Params: { "purchase_option_id": 1 }
   def purchase
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from User::AlreadyPurchasedError, with: :render_method_not_allowed
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def render_not_found_response(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def render_method_not_allowed(exception)
+    render json: { error: exception.message }, status: 405
+  end
 end
