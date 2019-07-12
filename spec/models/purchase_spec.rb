4c94fa1a786215292b39af0a416525b205293308
Andres Bernardi <cito.bernardi@gmail.com>
2019-07-12 12:15:43 +0200

    adds unit testing

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
-  # test alive? method
+  # SETUP
+  let(:purchase) { create(:purchase_movie) }
+  let(:old_purchase) { create(:old_purchase) }
+
+  # TEST SUITE
+  it "is alive for 48hs" do
+    expect(purchase.alive?).to eq(true)
+  end
+
+  it "is not alive after 48hs" do
+    expect(old_purchase.alive?).to eq(false)
+  end
+
+  it "remaining time is approximately 2880 minutes (2 days)" do
+    expect(purchase.remaining_time).to be_within(2878).of(2880)
+  end
+
+  it "remaining time is zero minutes after two days" do
+    expect(old_purchase.remaining_time).to eq(0)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,35 +2,25 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let(:user) { User.create(email: "user_test@testing.com") }
-  let(:movie_content) do 
-    Movie.create(
-      title: "Perdido en ti", 
-      plot: "Lorem ipsum dolor sit amet.")
-  end
-  let(:purchase_option_hd) { PurchaseOption.create(price: 2.99, video_quality: "HD", content: movie_content) }
+  # SETUP
+  let(:user) { create(:user_with_purchases) }
 
+  # TEST SUITE
   context "associations" do
     it { should have_many(:purchases) } 
   end
 
   it "can purchase a content" do
-    user.purchase(purchase_option_hd)
-
-    expect(user.library.size).to eql(1)
+    expect(user.library.size).to eql(4)
   end
 
   it "exclude expired purchase from libray" do
-    user.purchase(purchase_option_hd)
-
     user.purchases.first.update(created_at: 3.days.ago)
 
-    expect(user.library.size).to eql(0)
+    expect(user.library.size).to eql(3)
   end
 
   it "can't purchase a content that is in the library" do
-    user.purchase(purchase_option_hd)
-
-    expect{ user.purchase(purchase_option_hd) }.to raise_error(User::AlreadyPurchasedError)
+    expect{ user.purchase(user.library.first.purchase_option) }.to raise_error(User::AlreadyPurchasedError)
   end
 end
